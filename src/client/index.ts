c9d0f45dbe49ca79ee40b06c740d6fa8fe2aee71
hughfenghen <hughfenghen@gmail.com>
2022-03-31 17:38:46 +0800

    fix: require('http') 用 webpack打包报错

--- src/client/index.ts
+++ src/client/index.ts
@@ -68,8 +68,9 @@ function createDefAgent (baseUrl: string) {
       return (await res.json())[RPCKey.Return]
     } else if (typeof global === 'object') {
       // node env
+      const httpStr = 'http'
       // eslint-disable-next-line
-      const http = require('http') as typeof import('http')
+      const http = await import(httpStr) as typeof import('http')
       return await new Promise((resolve, reject) => {
         const url = new URL(`http://${baseUrl.replace(/^\/*|\/*$/g, '')}/${serviceName}/${methodName}`)
         const options = {

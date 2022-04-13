a8c453c046fdf16b4e07e7f10cf29020a02c75ca
hughfenghen <hughfenghen@gmail.com>
2022-04-13 17:59:15 +0800

    fix: 业务方项目中类型错误导致生成 schem 失败

--- src/bin/dts-to-schema.ts
+++ src/bin/dts-to-schema.ts
@@ -9,6 +9,8 @@ export function dts2JSONSchema (code: string, typeName: string): TSchema | null 
   const generator = TJS.buildGenerator(
     prj.getProgram().compilerObject as any,
     {
+      include: ['rpc-definition.ts'],
+      ignoreErrors: true,
       required: true,
       ref: false
     },
